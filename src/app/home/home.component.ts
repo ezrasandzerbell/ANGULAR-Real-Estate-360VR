19583c53d2506f2ed9137ba7b0a30e70fe58b157
ezra sandzer-bell <equilibriumarts@gmail.com>
2018-01-05 15:24:15 -0800

    style binding

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -11,8 +11,11 @@ export class HomeComponent implements OnInit {
   address = "12345 SE Belmont";
   firstpic = "https://static.secure.website/wscfus/10127251/2981249/highland-farm-mris-5-w1500-o.jpg";
   statsArray = ["4 Bedrooms", "3.5 Bathrooms", "4,000 S.F."];
-  paralaxOne = "background: linear-gradient(rgba(0,0,0,0.5), rgba(0,0,0,.5)), url(http://www.desktop-screens.com/data/out/54/2974479-house-wallpapers.jpg); height: 400px; background-attachment: fixed; background-position: center; background-repeat: no-repeat; background-size: cover;";
-  matterportEmbed = "https://my.matterport.com/show/?m=JGPnGQ6hosj&play=1&qs=1";
+  parallax1 = {'background': 'linear-gradient(rgba(0,0,0,0.5), rgba(0,0,0,.5)), url(http://www.desktop-screens.com/data/out/54/2974479-house-wallpapers.jpg)', 'height': '400px', 'background-attachment': 'fixed', 'background-position': 'center', 'background-repeat': 'no-repeat', 'background-size': 'cover'}
+
+  parallax2 = {'background': 'linear-gradient(rgba(0,0,0,0.1), rgba(0,0,0,.1)), url(https://images.unsplash.com/photo-1481277542470-605612bd2d61)', 'height': '100vh', 'background-attachment': 'fixed', 'background-position': 'center', 'background-repeat': 'no-repeat', 'background-size': 'cover'}
+
+  'matterportEmbed' = "https://my.matterport.com/show/?m=JGPnGQ6hosj&play=1&qs=1";
 
 
   constructor(public sanitizer: DomSanitizer) { }
